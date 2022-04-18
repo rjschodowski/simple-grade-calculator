4139926287968431de438614286f3b81c446b738
R.J. Schodowski <37779010+rjschodowski@users.noreply.github.com>
2022-04-18 15:01:24 -0400

    Class information completed

--- apstudent.hpp
+++ apstudent.hpp
@@ -1,6 +1,13 @@
-#include "student.h"
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+#include<iomanip>
+using namespace std; 
 
-class APstudent: private Student {
+class APstudent {
+  int idNum;
+  string firstName;
+  string lastName;
   float aptrigonometryGrade, apEnglishGrade, apSpanishGrade, apChemistryGrade, roboticsGrade, apGymGrade;
   float gpa;
   char l_grade;
@@ -18,5 +25,40 @@ void calculate() {
   } else {
     l_grade = 'E';
   }
+}
+
+public:
+  void getData() {
+    cout << "Enter student's ID number: ";
+    cin >> idNum;
+    cout << "Enter student's first name: ";
+    cin >> firstName;
+    cout << "Enter student's last name: ";
+    cin >> lastName;
+    cout << "Enter grade percentage for AP trigonometry: ";
+    cin >> aptrigonometryGrade;
+    cout << "Enter grade percentage for AP English: ";
+    cin >> apEnglishGrade;
+     cout << "Enter grade percentage for AP Spanish: ";
+    cin >> apSpanishGrade;
+    cout << "Enter grade percentage for AP chemistry: ";
+    cin >> apChemistryGrade;
+    cout << "Enter grade percentage for robotics: ";
+    cin >> roboticsGrade;
+    cout << "Enter grade percentage for AP gym: ";
+    cin >> apGymGrade;
+    calculate();
+  }
+
+void showData() {
+  cout << "\n\nName of student:\t\t\t" << firstName << " " << lastName << endl;
+  cout << "Student ID number:\t\t\t" << idNum << endl;
+  cout << "Grade in AP trigonometry:\t" << aptrigonometryGrade << endl;
+  cout << "Grade in AP English:\t\t" << apEnglishGrade << endl;
+  cout << "Grade in AP Spanish:\t\t" << apSpanishGrade << endl;
+  cout << "Grade in AP chemistry:\t\t" << apChemistryGrade << endl;
+  cout << "Grade in robotics:\t\t\t" << roboticsGrade << endl;
+  cout << "Grade in AP gym:\t\t\t" << apGymGrade << endl;
+  cout << "\nStudent's gpa:\t\t\t\t" << setprecision(4) << gpa << endl;
 }
 };
